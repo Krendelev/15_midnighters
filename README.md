34f549c9c330c3022428d51e9464c63303491d6a
Krendelev <krendelev@gmail.com>
2018-09-21 07:47:24 +0300

    implement main feature

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Let's find who's coding instead of sleeping :)
 
-# Project Goals
+## How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+## Quick Start
+
+```bash
+$ python seek_dev_nighters.py
+Guido
+Raymond
+Tim
+Barry
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,28 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import itertools
+import datetime
+import requests
+import pytz
 
-def get_midnighters():
-    pass
+
+def load_attempts(url):
+    for page_num in itertools.count(start=1, step=1):
+        response = requests.get(url, params={'page': page_num})
+        if response.status_code != requests.codes.ok:
+            break
+        for record in response.json()['records']:
+            yield record
+
+
+def get_midnighters(attempts):
+    midnighters = set()
+    for attempt in attempts:
+        tz = pytz.timezone(attempt['timezone'])
+        time = datetime.datetime.fromtimestamp(attempt['timestamp'], tz=tz)
+        if 0 <= time.hour <= 6:
+            midnighters.add(attempt['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    print(*get_midnighters(load_attempts(url)), sep='\n')
